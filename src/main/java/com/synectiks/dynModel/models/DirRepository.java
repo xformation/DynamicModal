151cac0914d042370b35f3908ac0c588c8ea5d0a
rajeshupadhyay <rajeshupadhyay@girnarsoft.com>
2020-04-11 17:55:47 +0530

    Remove dynamic classes

--- src/main/java/com/synectiks/dynModel/models/DirRepository.java
+++
@@ -1,9 +0,0 @@
-package com.synectiks.dynModel.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirRepository extends JpaRepository<Dir, Long> {
-
-}
--- src/main/java/com/synectiks/dynModel/models/DirsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.synectiks.dynModel.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirsRepository extends JpaRepository<Dirs, Long> {
-
-}
